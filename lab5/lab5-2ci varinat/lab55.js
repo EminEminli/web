3a6586396f69332f4eecab30b1a7a8472996cb06
Emin Eminli <101602659+EminEminli@users.noreply.github.com>
2022-04-19 09:20:54 +0400

    Update lab55.js

--- lab5/lab5-2ci varinat/lab55.js
+++ lab5/lab5-2ci varinat/lab55.js
@@ -3,9 +3,8 @@ function fun() {
     a=Number(document.forma.a.value);
     b=Number(document.forma.b.value);
     x=Number(document.forma.x.value);
-    z=Math.pow(x,2)*Math.pow(Math.tan(Math.pow(x+b,2)),2)+a/Math.sqrt(x+b);
-w=(b*Math.pow(x,2)-a*y)/(Math.pow(e,a*x)-1);
-
+   y=Math.sqrt(x+1)*(x+6)/b-Math.pow(Math.sin(x+a),2);
+w=Math.sqrt((x*b)/a)+Math.pow(y,2)*Math.pow(Math.cos(Math.pow(x+b),2),2);
 
     document.forma.y.value=y;
     document.forma.w.value=w;
