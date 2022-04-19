37d52eafd19e6f4abbde11882809ea33d6bb4b2d
Emin Eminli <101602659+EminEminli@users.noreply.github.com>
2022-04-19 09:20:01 +0400

    Update lab5.js

--- lab5/lab5-2ci varinat/lab5.js
+++ lab5/lab5-2ci varinat/lab5.js
@@ -2,8 +2,8 @@ var a,b,y,x,w;
 a= parseFloat(prompt("Enter a ",""));
 b= parseFloat(prompt("Enter b ",""));
 x= parseFloat(prompt("Enter x ",""));
-y=Math.pow(x,2)*Math.pow(Math.tan(Math.pow(x+b,2)),2)+a/Math.sqrt(x+b);
-w=(b*Math.pow(x,2)-a*y)/(Math.pow(e,a*x)-1);
+y=Math.sqrt(x+1)*(x+6)/b-Math.pow(Math.sin(x+a),2);
+w=Math.sqrt((x*b)/a)+Math.pow(y,2)*Math.pow(Math.cos(Math.pow(x+b),2),2);
 
 alert("y="+y);
 alert("w="+w);
